a638804759f417d7ac073c8f3dc3120712bcd7dd
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-06-18 00:56:31 -0500

    started to work with container.js to be able to render to the dom

--- public/container.js
+++ public/container.js
@@ -1,2 +1,13 @@
 //socket declare to connect front end to server 
-const socket = io()
+const socket = io();
+// declare let variables to get the param from html file 
+let message = document.getElementById('message');
+let username = document.getElementById('username');
+let button = document.getElementById('send');
+let output = document.getElementById('output');
+let actions = document.getElementById('actions');
+
+btn.addEventListener('click', function () {
+    console.log('click');
+    
+})
