283027e4d29f5ffaf8ecfa2793bae7e48da91fc4
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-06-17 23:10:59 -0500

    set up server to connect with public folder

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 const port = 3000
 
+//connection to files in public folder 
+app.use(express.static(path.join(__dirname, 'public')))
+//server config 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
