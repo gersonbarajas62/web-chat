e0dab4ddd6fce0132cee70cc81bc64bb2e798048
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-06-17 23:25:27 -0500

    configurating socket.io

--- server.js
+++ server.js
@@ -10,4 +10,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 //server config 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+const server = app.listen(port, () => console.log(`Listening on port ${port}!`))
+
+const SocketIO = require('socket.io')
+const io = SocketIO(server);
+
+//webSokets
+io.on('connection', () => {
+    console.log(connected)
+})
+
